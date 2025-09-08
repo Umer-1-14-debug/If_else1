f04aa11860ef98a181876025004771ae0f6bb714
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-09-08 19:54:58 +0500

    Add files via upload

---
+++ number_greaterorless.py
@@ -0,0 +1,6 @@
+number=float(input("Enter a number."))
+second_number=float(input("Enter the second number."))
+if number>second_number:
+    print(number," is greater than ",second_number)
+else:
+    print(second_number," is greater than ",number)
