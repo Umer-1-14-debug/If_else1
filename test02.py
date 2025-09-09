c8c85c1acbba5854950a6841a378fc0aa14df4fd
Umer-1-14-debug <m.umer0000013@gmail.com>
2025-09-09 18:10:37 +0500

    Add files via upload

---
+++ test02.py
@@ -0,0 +1,6 @@
+password=123456
+check_password=int(input("Enter the password."))
+if password==check_password:
+    print("Hurry! The password is correct.")
+else:
+    print("Alas! The password is incorrect.")
